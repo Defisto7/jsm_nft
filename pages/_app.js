d334efe280b2118c4e3f5a937a1a7f940d627563
Dima Yanovskji <criska7@gmail.com>
2023-03-29 14:14:29 +0300

    Master
    add NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <h1>NAVBAR</h1>
+      <Navbar />
       <Component {...pageProps} />;
-      <h1>NAVBAR</h1>
+      <Footer />
     </div>
+
+    <Script src="https://kit.fontawesome.com/937504f56d.js" crossOrigin="anonymous" />
   </ThemeProvider>
 );
 export default App;
